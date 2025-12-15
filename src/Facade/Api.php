5fb4b17ad65404c69ca668f587ab9c62000b54d4
Korneel Eeckhout <korneel@pienter.tech>
2025-12-15 14:41:42 +0100

    docs: add @method annotations to Api facade for PHPStan inference
    Add PHPDoc @method annotations documenting all public methods from the
    Router contract. This enables PHPStan to properly infer types and provide
    better IDE support when using static facade methods.

--- src/Facade/Api.php
+++ src/Facade/Api.php
@@ -5,6 +5,16 @@ namespace Tnt\InternalApi\Facade;
 use Oak\Facade;
 use Tnt\InternalApi\Router\Router;
 
+/**
+ * @method static void get(string $pattern, callable $controller)
+ * @method static void post(string $pattern, callable $controller)
+ * @method static void put(string $pattern, callable $controller)
+ * @method static void delete(string $pattern, callable $controller)
+ * @method static array getRoutes()
+ * @method static void route(\dry\http\Request $request)
+ *
+ * @see Router
+ */
 class Api extends Facade
 {
 	protected static function getContract(): string
